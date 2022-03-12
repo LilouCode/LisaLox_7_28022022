16842398db73788765796ce53cf26d2b8cdcd424
Lisa L <lilas.lisa@hotmail.com>
2022-03-12 18:36:09 +0100

    Main: remove algo 1

--- script/components/search.js
+++ script/components/search.js
@@ -1,20 +1,6 @@
 import { recipes } from "../data/recipes.js";
 import { createCard, createListIngredients } from "./grid.js";
 
-function filtrerParRecherche(obj){
-  obj.ingredients.includes('i')
-}
-const recipeFiltered = recipes.filter(filtrerParRecherche);
-console.log(recipeFiltered)
-recipes.forEach((recipes) => {
-  console.log('La recette pour faire un(e) super '+recipes.name)
-});
-const oldRecipes = ['chocolat', 'coco', 'banane', 'ananas', 'haricots'];
-const newRecipes = oldRecipes.filter(oldRecipes => oldRecipes.includes('co'));
-console.log(newRecipes);
-
-
-// recipes.forEach(recipes.in)
 const searchBar = document.getElementById("search__input");
 searchBar.addEventListener("input", function () {
   if (searchBar.value.length >= 3) {
