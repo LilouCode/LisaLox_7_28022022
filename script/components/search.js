1090ac9ecfe9c662279a9e92bc1e7f71b63f52e2
Lisa L <lilas.lisa@hotmail.com>
2022-03-12 18:02:52 +0100

    Algo: implementation init (main)

--- script/components/search.js
+++ script/components/search.js
@@ -1,7 +1,31 @@
+import { recipes } from "../data/recipes.js";
+import { createCard, createListIngredients } from "./grid.js";
+
+function filtrerParRecherche(obj){
+  obj.ingredients.includes('i')
+}
+const recipeFiltered = recipes.filter(filtrerParRecherche);
+console.log(recipeFiltered)
+recipes.forEach((recipes) => {
+  console.log('La recette pour faire un(e) super '+recipes.name)
+});
+const oldRecipes = ['chocolat', 'coco', 'banane', 'ananas', 'haricots'];
+const newRecipes = oldRecipes.filter(oldRecipes => oldRecipes.includes('co'));
+console.log(newRecipes);
+
+
+// recipes.forEach(recipes.in)
 const searchBar = document.getElementById("search__input");
 searchBar.addEventListener("input", function () {
   if (searchBar.value.length >= 3) {
     const saisie = searchBar.value;
+    grid.innerHTML = "";
     console.log(saisie);
+  } else {
+    if (grid.innerHTML === "")
+      recipes.forEach((recipes) => {
+        createCard(recipes);
+        createListIngredients(recipes);
+      });
   }
 });
