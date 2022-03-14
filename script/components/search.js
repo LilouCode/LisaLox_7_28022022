a0e4ab2b830de131bca1a88abe52bc5191d56fee
Lisa L <lilas.lisa@hotmail.com>
2022-03-14 16:25:32 +0100

    Algo 2: init

--- script/components/search.js
+++ script/components/search.js
@@ -1,7 +1,26 @@
 const searchBar = document.getElementById("search__input");
 searchBar.addEventListener("input", function () {
+  const saisie = searchBar.value.toLocaleLowerCase();
+  const messageNoResult = `<p id="message-no-result">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</p>`;
   if (searchBar.value.length >= 3) {
-    const saisie = searchBar.value;
+    grid.innerHTML = "";
     console.log(saisie);
+    const results = [];
+    if (results.length > 0) {
+      results.forEach((results) => {
+        createCard(results);
+        createListIngredients(results);
+      });
+    } else {
+      grid.innerHTML = messageNoResult;
+    }
+  } else {
+    if (grid.innerHTML === "" || messageNoResult) {
+      grid.innerHTML = "";
+      recipes.forEach((recipes) => {
+        createCard(recipes);
+        createListIngredients(recipes);
+      });
+    }
   }
 });
