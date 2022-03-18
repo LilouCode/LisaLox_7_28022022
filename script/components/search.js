e6ab493c96eb4c5ed3be0948428153ce17d12686
Lisa L <lilas.lisa@hotmail.com>
2022-03-18 15:15:55 +0100

    nativeSearch()

--- script/components/search.js
+++ script/components/search.js
@@ -1,51 +1,16 @@
 import { recipes } from "../data/recipes.js";
-import { listIngredientsDrop, showElements} from "./searchTags.js";
+import { listIngredientsDrop, showElements } from "./searchTags.js";
 
-function makeTable(str) {
-  let table = new Array(str.length);
-  let maxPrefix = 0;
-
-  table[0] = 0;
-
-  for (let i = 1; i < str.length; i++) {
-    while (maxPrefix > 0 && str.charAt(i) !== str.charAt(maxPrefix)) {
-      maxPrefix = table[maxPrefix - 1];
-    }
-
-    if (str.charAt(maxPrefix) === str.charAt(i)) {
-      maxPrefix++;
-    }
-    table[i] = maxPrefix;
-  }
-  return table;
-}
-
-function kmpMatching(str, word) {
-  let prefixes = makeTable(word);
-  let matches = [];
-
-  let j = 0;
-  let i = 0;
-  while (i < str.length) {
-    if (str.charAt(i) === word.charAt(j)) {
-      i++;
-      j++;
-    }
-
-    if (j === word.length) {
-      matches.push(i - j);
-
-      j = prefixes[j - 1];
-    } else if (str.charAt(i) !== word.charAt(j)) {
-      if (j !== 0) {
-        j = prefixes[j - 1];
-      } else {
-        i++;
-      }
+//
+function nativeSearch(str, word) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] !== str[i + j]) break;
+      if (j === word.length - 1) count++;
     }
   }
-
-  return matches;
+  return count;
 }
 const searchBar = document.getElementById("search__input");
 searchBar.addEventListener("input", function () {
@@ -55,28 +20,28 @@ searchBar.addEventListener("input", function () {
     grid.innerHTML = "";
     console.log(saisie);
     const results = [];
-    for (let i = 0; i < recipes.length; i++){
+    for (let i = 0; i < recipes.length; i++) {
       
       if (
-        kmpMatching(recipes[i].name.toLocaleLowerCase(), saisie).length !== 0 ||
-        kmpMatching(recipes[i].description.toLocaleLowerCase(), saisie).length !== 0 ||
-        kmpMatching(recipes[i].ingredients.some(i => i.ingredient),saisie).length !== 0){
-        results.push(recipes[i]);
+        nativeSearch(recipes[i].name.toLocaleLowerCase(), saisie) == 1 ||
+        nativeSearch(recipes[i].description.toLocaleLowerCase(), saisie) == 1 ){
+        const ajout = results.length;
+        results[ajout] = recipes[i];
       }
     }
-    
+
     if (results.length > 0) {
       listIngredientsDrop.innerHTML = "";
-      showElements(results)
+      showElements(results);
     } else {
       grid.innerHTML = messageNoResult;
-      listIngredientsDrop.innerHTML="";
+      listIngredientsDrop.innerHTML = "";
     }
   } else {
     if (grid.innerHTML === "" || messageNoResult) {
       grid.innerHTML = "";
-      listIngredientsDrop.innerHTML="";
-      showElements(recipes)
+      listIngredientsDrop.innerHTML = "";
+      showElements(recipes);
     }
   }
 });
