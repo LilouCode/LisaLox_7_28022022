681c31f89d62429f82603ea9f505b5afdfaf8a2a
Lisa L <lilas.lisa@hotmail.com>
2022-03-21 17:03:41 +0100

    Tags: filters & input okay

--- script/components/search.js
+++ script/components/search.js
@@ -1,31 +1,98 @@
 import { recipes } from "../data/recipes.js";
-import { listIngredientsDrop, showElements} from "./searchTags.js";
+import { tableauTagsIngredients, listIngredientsDrop, showElements} from "./searchTags.js";
 
 const searchBar = document.getElementById("search__input");
+const searchTagZone = document.getElementById("search-tag");
+let results = [];
 searchBar.addEventListener("input", function () {
   const saisie = searchBar.value.toLocaleLowerCase();
   const messageNoResult = `<p id="message-no-result">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</p>`;
   if (searchBar.value.length >= 3) {
     grid.innerHTML = "";
     console.log(saisie);
-    const results = recipes.filter(
+    results = recipes.filter(
       (recipes) =>
         recipes.name.toLocaleLowerCase().includes(saisie) ||
         recipes.description.toLocaleLowerCase().includes(saisie) ||
         recipes.ingredients.some((i) => i.ingredient.toLocaleLowerCase().includes(saisie))
     );
+
     if (results.length > 0) {
       listIngredientsDrop.innerHTML = "";
-      showElements(results)
+      showElements(results);
     } else {
       grid.innerHTML = messageNoResult;
-      listIngredientsDrop.innerHTML="";
+      listIngredientsDrop.innerHTML = "";
     }
   } else {
     if (grid.innerHTML === "" || messageNoResult) {
       grid.innerHTML = "";
-      listIngredientsDrop.innerHTML="";
-      showElements(recipes)
+      listIngredientsDrop.innerHTML = "";
+      showElements(recipes);
     }
+  }
+  return results;
+});
+
+///////////////
+const selectTags = document.getElementById("list-ingredients");
+let resultsI = [];
+
+// ecoutes ajouts tags
+selectTags.addEventListener("click", function () {
+  if (tableauTagsIngredients.length > 1) {
+    console.log("la liste des tags contient plus d'un élément")
+    //si la liste des tags contient plus d'un élément
+    if (results.length > 0) {
+      //si le tableau est déjà filtré par la barre de recherche
+      console.log("le tableau est déjà filtré par la barre de recherche")
+      resultsI = results.filter((results) => results.ingredients.some((a) => a.ingredient.includes(tableauTagsIngredients[0])));
+      for (let i = 1; i < tableauTagsIngredients.length; i++) {
+        resultsI = resultsI.filter((results) => results.ingredients.some((a) => a.ingredient.includes(tableauTagsIngredients[i])));
+      }
+    } else {
+      //si le tableau n'a pas encore été filtré
+      console.log("le tableau n'a pas encore été filtré par la barre de recherche")
+      resultsI = recipes.filter((recipes) => recipes.ingredients.some((a) => a.ingredient.includes(tableauTagsIngredients[0])));
+      for (let i = 1; i < tableauTagsIngredients.length; i++) {
+        resultsI = resultsI.filter((results) => results.ingredients.some((a) => a.ingredient.includes(tableauTagsIngredients[i])));
+      }
+    }
+    showElements(resultsI);
+  } else {
+    console.log("la liste des tags ne contient pas plus d'un élément")
+    // si la liste des tags ne contient pas plus d'un élément
+    if (results.length > 0) {
+      console.log("le tableau est déjà filtré par la barre de recherche")
+      //si le tableau est déjà filtré par la barre de recherche
+      resultsI = results.filter((results) => results.ingredients.some((i) => i.ingredient.includes(tableauTagsIngredients)));
+    } else {
+      //si le tableau n'a pas encore été filtré
+      console.log("le tableau n'a pas encore été filtré par la barre de recherche")
+      resultsI = recipes.filter((recipes) => recipes.ingredients.some((i) => i.ingredient.includes(tableauTagsIngredients)));
+    }
+    showElements(resultsI);
+  }
+});
+//ecoute suppression tags
+searchTagZone.addEventListener("click", function () {
+  if (tableauTagsIngredients.length > 0) {
+    if (results.length > 0) {
+      resultsI = results.filter((results) => results.ingredients.some((i) => i.ingredient.includes(tableauTagsIngredients)));
+    } else {
+      resultsI = recipes.filter((recipes) => recipes.ingredients.some((i) => i.ingredient.includes(tableauTagsIngredients)));
+    }
+    listIngredientsDrop.innerHTML = "";
+    grid.innerHTML = "";
+    showElements(resultsI);
+    return resultsI;
+  } else if (tableauTagsIngredients.length < 1 && results.length > 0) {
+    listIngredientsDrop.innerHTML = "";
+    grid.innerHTML = "";
+    showElements(results);
+  } else if (tableauTagsIngredients.length < 1 && results.length < 1) {
+    listIngredientsDrop.innerHTML = "";
+    grid.innerHTML = "";
+    showElements(recipes);
   }
 });
--- script/components/searchTags.js
+++ script/components/searchTags.js
@@ -1,10 +1,19 @@
 import { recipes } from "../data/recipes.js";
 import { createCard, createListIngredients } from "./grid.js";
 
+//DOM
 export const listIngredientsDrop = document.getElementById("list-ingredients");
-let listIngredients = [];
-let BackToListIngredients;
+const inputDropIngredients = document.getElementById("input-ingredients");
 
+//Tableaux :
+export const tableauTagsIngredients = [];
+export let listIngredients = [];
+export let FinalListIngredients = [];
+let sortedListIngredients = [];
+
+//Fonctions
+
+//Création d'une liste d'ingrédients à partir d'une recette, trier par ordre alphabétique avec supression des doublons
 export function MakeTheIngredientsList(objet) {
   objet.forEach((objet) => {
     const ingredients = objet.ingredients;
@@ -14,9 +23,10 @@ export function MakeTheIngredientsList(objet) {
       return listIngredients;
     });
   });
-  console.log(listIngredients);
+  //supression des doublons
   const uniqueSetIngredients = new Set(listIngredients);
-  BackToListIngredients = [...uniqueSetIngredients];
+  sortedListIngredients = [...uniqueSetIngredients];
+  //tri par odre alphabétique
   function trierParOdreAlphabethique(a, b) {
     if (a.toLowerCase() < b.toLowerCase()) {
       return -1;
@@ -24,25 +34,60 @@ export function MakeTheIngredientsList(objet) {
       return 1;
     }
   }
-  BackToListIngredients.sort(trierParOdreAlphabethique);
-  console.log(BackToListIngredients);
-  return BackToListIngredients;
+  sortedListIngredients.sort(trierParOdreAlphabethique);
+  FinalListIngredients = sortedListIngredients;
+  if (tableauTagsIngredients.length > 0) {
+    console.log("c'est ici");
+    tableauTagsIngredients.forEach((el) => {
+      console.log(tableauTagsIngredients.length);
+      let index = sortedListIngredients.indexOf(el);
+      sortedListIngredients.splice(index, 1);
+    });
+  }
+
+  console.log("FinalListIngredients: " + FinalListIngredients); //test sortie
+  return FinalListIngredients;
 }
 
+inputDropIngredients.addEventListener("input", function () {
+  let saisieDropIngredients = inputDropIngredients.value.toLocaleLowerCase();
+  console.log(saisieDropIngredients);
+  let filtredListIngredients = sortedListIngredients.filter((recipes) => recipes.toLocaleLowerCase().includes(saisieDropIngredients));
+  if (filtredListIngredients.length > 0) {
+    FinalListIngredients = filtredListIngredients;
+    listIngredientsDrop.innerHTML= "";
+    FinalListIngredients.forEach((element) => {
+      createAnIngredientItem(element);
+    });
+  }
+  console.log("FinalListIngredients: " + FinalListIngredients);
+});
+// Création d'un élément Tag du DOM à partir d'un évenement (click dans la liste des ingrédients)
 function createATag(item) {
-  const tagZone = document.getElementById("search-tag");
-  const tag = document.createElement("button");
+  //DOM
+  const tagZone = document.getElementById("search-tag"); //repère dans le DOM
+  const tag = document.createElement("button"); //création
+  // ajouts des attributs:
   tag.classList.add("tag");
   tag.classList.add("tag--ingredients");
+  tag.setAttribute("id", item);
   tag.innerHTML = item;
   tagZone.appendChild(tag);
+  //évenements (remove tag)
   tag.addEventListener("click", function () {
     createAnIngredientItem(item);
     tagZone.removeChild(tag);
+    let index = tableauTagsIngredients.indexOf(item);
+    tableauTagsIngredients.splice(index, 1);
+    console.log("supression tag, liste tableauTagsIngredients: " + tableauTagsIngredients);
+    return tableauTagsIngredients;
   });
+  tableauTagsIngredients.push(item);
+  console.log("liste tableauTagsIngredients: " + tableauTagsIngredients);
   return tagZone;
 }
 
+// création d'un bouton dans la zone de selection des tag (dropdown) pour chaque ingrédients à partir de la liste des ingrédients:
 function createAnIngredientItem(item) {
   const itemList = document.createElement("button");
   itemList.classList.add("dropdown__list__item");
@@ -50,23 +95,28 @@ function createAnIngredientItem(item) {
   itemList.setAttribute("id", item);
   listIngredientsDrop.appendChild(itemList);
   itemList.addEventListener("click", function () {
+    inputDropIngredients.value = "";
     createATag(item);
-    listIngredientsDrop.removeChild(itemList);
   });
   return listIngredientsDrop;
 }
 
+//création de la liste + créations des Cards avec affichage des éléments dans la grid
 export function showElements(objet) {
+  // départ
+  grid.innerHTML = "";
   listIngredients = [];
+  listIngredientsDrop.innerHTML = "";
+  //reprise
   MakeTheIngredientsList(objet);
-  console.log(BackToListIngredients);
-  BackToListIngredients.forEach((element) => {
+  console.log(FinalListIngredients);
+  FinalListIngredients.forEach((element) => {
     createAnIngredientItem(element);
   });
-
   objet.forEach((el) => {
     createCard(el);
     createListIngredients(el);
   });
 }
+
 showElements(recipes);
